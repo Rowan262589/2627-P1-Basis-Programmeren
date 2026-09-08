e5d43bd8163e0859d664f4041285b1f5bbb641a6
Rowan262589 <260108@student.glu.nl>
2026-09-08 12:16:53 +0200

    code wijziging

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,7 +1,107 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
+background(220);
+
+  fill('red');
+  rect(10, 10, 100, 30);
+fill('white');
+rect(10, 40, 100, 30)
+fill('blue');
+rect(10, 70, 100, 30);
+fill('black');
+  text('Rowan',10, 150, 30);
+square(200, 10, 50);
+fill('white');
+  square(250, 10, 50);
+fill('black');
+square(300, 10, 50);
+fill('white');
+square(200, 60, 50);
+square(300, 60, 50);
+fill('black');
+square(250, 60, 50);
+square(200, 110, 50);
+square(300, 110, 50);
+fill('white');
+square(250, 110, 50);
+fill('black')
+  square(10, 200, 50);
+triangle(9, 200, 61, 200, 35, 160)
+  rect(10, 260, 30,100)
+  fill('red')
+circle(25, 275, 25)
+fill('yellow')
+circle(25, 308, 25)
+fill('green')
+  circle(25, 340, 25)
+fill('gray')
+  rect(20, 360, 10, 100)
+fill('white')
+  square(250, 200, 75, 10)
+fill('black')
+  circle(265, 215, 20,)
+ circle(288, 238, 20,)
+ circle(310, 260, 20,)
+strokeWeight(0)
+  fill('red')
+rect(400, 400, 150, 30)
+rect(370, 430, 270, 30)
+fill('#875526')
+  rect(370, 460, 100, 30)
+rect(410, 490, 30, 30)
+rect(410, 520, 60, 30)
+rect(340, 490, 30, 60)
+rect(340, 550, 70, 30)
+fill('black')
+rect(520, 460, 30, 60)
+rect(550, 520, 30, 30)
+rect(520, 550, 120, 30)
+fill('#c99d73')
+rect(370, 490, 40, 60)
+rect(470, 460, 50, 30)
+rect(440, 490, 80, 30)
+rect(470, 520, 80, 30)
+  rect(580, 520, 80, 30)
+rect(550, 490, 80, 30)
+rect(410, 550, 110, 30)
+rect(410, 580, 200, 30)
+fill('red')
+  rect(490, 610, 90, 30)
+ rect(380, 610, 60, 30)
+rect(350, 640, 90, 30)
+ rect(320, 670, 120, 30)
+ rect(380, 700, 30, 30)
+ rect(490, 640, 60, 30)
+rect(580, 640, 60, 60)
+ rect(640, 670, 60, 60)
+rect(610, 700, 30, 30)
+  fill('blue')
+rect(440, 610, 50, 90)
+rect(550, 640, 30, 60)
+rect(490, 670, 60, 120)
+fill("yellow")
+rect(460, 700, 30, 30)
+rect(550, 700, 30, 30)
+fill('blue')
+  rect(410, 700, 50, 90)
+rect(460, 730, 30, 60)
+rect(550, 730, 60, 60)
+rect(580, 700, 30, 30)
+rect(410, 790, 200, 30)
+rect(380, 760, 90, 90)
+rect(550, 760, 90, 90)
+fill('#875526')
+rect(580, 850, 90, 60)
+rect(350, 850, 90, 60)
+rect(320, 880, 30, 30)
+rect(670, 880, 30, 30)
+fill('#c99d73')
+rect(320, 700, 60, 90)
+rect(350, 730, 60, 30)
+rect(640, 700, 60, 90)
+rect(610, 730, 60, 30)
+
 }
 
 function draw() {
-  background(220);
 }
